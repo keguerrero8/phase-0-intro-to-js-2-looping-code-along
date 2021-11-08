39a415a156be390d563369d6f734ffdb11d73ace
Kevin Guerrero <guerrk83z@gmail.com>
2021-11-07 20:37:50 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+function writeCards(arrayName, stringName) {
+    let newArray = []
+    let message = ""
+    for (let i =0 ; i < arrayName.length; i++) {
+        message = `Thank you, ${arrayName[i]}, for the wonderful ${stringName} gift!`
+        newArray.push(message)
+    }
+    return newArray
+}
+
+function countDown(number) {
+    let i = 10
+    while (i > -1) {
+        console.log(i)
+        i--
+    }
+}
+
+//console.log(writeCards(["Ada", "Brendan", "Ali"], "birthday"))
+//wrapGifts(gifts);
+//writeCards( [ 'Lisa', 'Kaitlin', 'Jan' ], 'surprise' ) 
